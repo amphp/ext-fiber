8c4f2698fb7eb0b25203897ebb7c433b495029ec
Aaron Piotrowski <aaron@trowski.com>
2020-09-02 11:33:14 -0500

    Improve stub

--- stub.php
+++ stub.php
@@ -2,15 +2,61 @@
 
 final class Fiber
 {
+    public const STATUS_INIT = 0;
+    public const STATUS_SUSPENDED = 1;
+    public const STATUS_RUNNING = 2;
+    public const STATUS_FINISHED = 3;
+    public const STATUS_DEAD = 4;
+
+    /**
+     * @param callable $callback Function to invoke when creating the Fiber.
+     * @param int|null $stack_size Maximum call stack depth.
+     */
     public function __construct(callable $callback, ?int $stack_size = null) { }
-    
+
+    /**
+     * @return int One of the Fiber status constants.
+     */
     public function status(): int { }
-    
+
+    /**
+     * Start the Fiber by invoking the callback given to the constructor with the given arguments.
+     *
+     * @param mixed ...$args
+     *
+     * @return mixed Value given to next {@see Fiber::suspend()} call or function return value if the fiber completes
+     *               execution.
+     *
+     * @throws Throwable If the fiber throws, the exception will be thrown from this call.
+     */
     public function start(...$args) { }
-    
-    public function resume($val = null) { }
-    
-    public function throw(\Throwable $e) { }
-    
-    public static function suspend($val = null) { }
+
+    /**
+     * @param mixed $value Value to return from {@see Fiber::suspend()}.
+     *
+     * @return mixed Value given to next {@see Fiber::suspend()} call or function return value if the fiber completes
+     *               execution.
+     *
+     * @throws Throwable If the fiber throws, the exception will be thrown from this call.
+     */
+    public function resume($value = null) { }
+
+    /**
+     * @param Throwable $exception Exception to throw from {@see Fiber::suspend()}.
+     *
+     * @return mixed Value given to next {@see Fiber::suspend()} call or function return value if the fiber completes
+     *               execution.
+     *
+     * @throws Throwable If the fiber throws, the exception will be thrown from this call.
+     */
+    public function throw(\Throwable $exception) { }
+
+    /**
+     * @param mixed $value
+     *
+     * @return mixed Value given to {@see Fiber::resume()} when resuming the fiber.
+     *
+     * @throws Error Thrown if not within a Fiber context.
+     */
+    public static function suspend($value = null) { }
 }
