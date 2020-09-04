90c4e4f076245ee979acf2026ed810aa965972d5
Aaron Piotrowski <aaron@trowski.com>
2020-09-04 09:44:36 -0500

    Remove missed stack argument info

--- src/fiber.c
+++ src/fiber.c
@@ -439,7 +439,6 @@ ZEND_METHOD(Fiber, __wakeup)
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_fiber_create, 0, 0, 1)
 	ZEND_ARG_CALLABLE_INFO(0, callable, 0)
-	ZEND_ARG_TYPE_INFO(0, stack_size, IS_LONG, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_fiber_getStatus, 0, 0, IS_LONG, 0)
