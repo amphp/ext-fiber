a7518b4a4dec40d9e0b4dd0b6afd4d09c7f8306a
Aaron Piotrowski <aaron@trowski.com>
2020-09-05 09:11:54 -0500

    Fix arginfo/zpp mismatch

--- src/fiber.c
+++ src/fiber.c
@@ -444,11 +444,11 @@ ZEND_END_ARG_INFO()
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_fiber_getStatus, 0, 0, IS_LONG, 0)
 ZEND_END_ARG_INFO()
 
-ZEND_BEGIN_ARG_INFO(arginfo_fiber_start, 0)
+ZEND_BEGIN_ARG_INFO_EX(arginfo_fiber_start, 0, 0, 0)
 	ZEND_ARG_VARIADIC_INFO(0, arguments)
 ZEND_END_ARG_INFO()
 
-ZEND_BEGIN_ARG_INFO_EX(arginfo_fiber_resume, 0, 0, 1)
+ZEND_BEGIN_ARG_INFO_EX(arginfo_fiber_resume, 0, 0, 0)
 	ZEND_ARG_INFO(0, value)
 ZEND_END_ARG_INFO()
 
