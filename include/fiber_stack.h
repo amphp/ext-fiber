7f9bb6a38e264b8ab91b39e6a6770b3ff66837c2
Aaron Piotrowski <aaron@trowski.com>
2021-03-27 16:59:30 -0500

    Remove some unneeded conditional defines

--- include/fiber_stack.h
+++ include/fiber_stack.h
@@ -27,36 +27,16 @@ zend_bool zend_fiber_stack_allocate(zend_fiber_stack *stack, unsigned int size);
 void zend_fiber_stack_free(zend_fiber_stack *stack);
 
 #if _POSIX_MAPPED_FILES
-#define ZEND_FIBER_MMAP 1
-
 #include <sys/mman.h>
 #include <limits.h>
-
-#ifndef MAP_ANONYMOUS
-#ifdef MAP_ANON
-#define MAP_ANONYMOUS MAP_ANON
-#else
-#undef ZEND_FIBER_MMAP
-#endif
-#endif
-
-#endif
-
-#if _POSIX_MEMORY_PROTECTION
-#define ZEND_FIBER_GUARD_PAGES 1
-#endif
-
-#ifndef ZEND_FIBER_GUARD_PAGES
-#define ZEND_FIBER_GUARD_PAGES 0
-#endif
-
-#ifdef ZEND_FIBER_MMAP
 #define ZEND_FIBER_PAGESIZE sysconf(_SC_PAGESIZE)
 #else
 #define ZEND_FIBER_PAGESIZE 4096
 #endif
 
-#define ZEND_FIBER_DEFAULT_STACK_SIZE ZEND_FIBER_PAGESIZE * (((sizeof(void *)) < 8) ? 512 : 2048)
+#define ZEND_FIBER_GUARD_PAGES 1
+
+#define ZEND_FIBER_DEFAULT_STACK_SIZE (ZEND_FIBER_PAGESIZE * (((sizeof(void *)) < 8) ? 512 : 2048))
 
 #endif
 
